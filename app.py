050d710005ce8dcf024ef62d188f92daffa485ce
S Koteswararao <srkotesh23@gmail.com>
2025-10-17 21:36:48 +0530

    update the latest changes in app.py

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ i = 0
 while i < 5:
     print("Iteration:", i)
     i += 1
+    
+print("Done with iterations.")
+# The above code concatenates '5' to the input string instead of adding numerically.
+
+# To fix the code, we need to convert the input to an integer before adding 5.
+num = int(input("Enter a number: "))
+result = num + 5
+print("Result is:", result)
